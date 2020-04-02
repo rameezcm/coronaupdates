aa74a40d8ac03b23d5df4186f9e7b25c58a33ca2
Your Name <you@example.com>
2020-04-02 13:40:10 +0200

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ class RameezDashboard extends HTMLElement {
 
     connectedCallback() {
         this.fetchfromServer();
-        console.log(this.answer);
         this.innerHTML = `<div class="checkout-panel">
         <div class="panel-body">
             <h2 class="title">Rameez Dashboard</h2>
@@ -40,6 +39,17 @@ class RameezDashboard extends HTMLElement {
                 <td id="critical" align="right"></td>
                 <td id="deathsPerOneMillion" align="right"></td>
                 </tr>
+                <tr>
+                <td>India</td>
+                <td  id="casesi" align="right"></td>
+                <td id="todayCasesi" align="right"></td>
+                <td id="deathsi" align="right"></td>
+                <td id="todayDeathsi"></td>
+                <td id="recoveredi" align="right"></td>
+                <td id="activei" align="right"></td>
+                <td id="criticali" align="right"></td>
+                <td id="deathsPerOneMillioni" align="right"></td>
+                </tr>
                 </tbody></table>
                 </label>   
             </div>
@@ -67,7 +77,23 @@ class RameezDashboard extends HTMLElement {
         this.querySelector("#active").innerText = this.answer.active
         this.querySelector("#critical").innerText = this.answer.critical
         this.querySelector("#deathsPerOneMillion").innerText = this.answer.deathsPerOneMillion
-        console.log(this.answer);
+        const response1 = await fetch("https://corona.lmao.ninja/countries/India", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'https://corona.lmao.ninja'
+            }
+        });
+        const json1 = await response1.json();
+        this.answer = json1;
+        this.querySelector("#casesi").innerText = this.answer.cases
+        this.querySelector("#todayCasesi").innerText = this.answer.todayCases
+        this.querySelector("#deathsi").innerText = this.answer.deaths
+        this.querySelector("#todayDeathsi").innerText = this.answer.todayDeaths
+        this.querySelector("#recoveredi").innerText = this.answer.recovered
+        this.querySelector("#activei").innerText = this.answer.active
+        this.querySelector("#criticali").innerText = this.answer.critical
+        this.querySelector("#deathsPerOneMillioni").innerText = this.answer.deathsPerOneMillion
     }
 }
 customElements.define("rameez-page", RameezDashboard)
