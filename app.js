d9edae03f158c021d7e542e897c06d396a29231d
Rameez CM <rameezcm88@gmail.com>
2020-04-03 11:41:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ class RameezDashboard extends HTMLElement {
     async fetchfromServer() {
         const response = await fetch("https://redutv-api.vg.no/corona/v1/sheets/norway-table-overview?region=county",{
             method: 'GET',
-            mode:'no=cors',
+            mode:'no-cors',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': 'https://rameezcm.github.io'
